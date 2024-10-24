ca4a8d3c16613005674ea318a4813c5058a924ef
Aleksei Dziuba <adzyuba@cern.ch>
2024-10-24 10:14:26 +0300

    simple reco into root-file

--- event_builder.C
+++ event_builder.C
@@ -20,6 +20,8 @@
 
 TString FILENAME = "mc_fadc.root";
 
+bool PRINT_RECO=false;
+
 //  double tau = 500.; // 2.0 MHz
 //  double tau = 10000.; // 0.1 MHz
 double TAU = 1000.; // 1.0 MHz
@@ -37,6 +39,7 @@ double z_anod = -50.;
 double Digi[125];
 
 TTree *tree;
+TTree *reco;
 TTree *mc_true;
 TFile *file;
 
@@ -233,7 +236,7 @@ bool get_sr( TH1F *h , int&  start_bin, int& end_bin, int& peak_bin, double& pea
   int t=pos-1;
   double height = peak - base;
   cv p80, p25; p80.t=0; p25.t=0; p80.v=peak; p25.v = base ;
-  while( h->GetBinContent(t)>base ){
+  while( h->GetBinContent(t)>base && t>0){
      energy += ( h->GetBinContent(t)-base );
      if( ( h->GetBinContent(t)-base ) / height < 0.80 && p80.t==0 ){
          p80.t = t ; p80.v = h->GetBinContent(t);
@@ -248,7 +251,7 @@ bool get_sr( TH1F *h , int&  start_bin, int& end_bin, int& peak_bin, double& pea
 
   //right
   t=pos+1;
-  while( h->GetBinContent(t)>base ){
+  while( h->GetBinContent(t)>base && t<2693){
      energy += ( h->GetBinContent(t)-base );
      t++;
   } end_bin = t-1; end = end_bin;
@@ -278,7 +281,6 @@ void findTPCtracks(){
   TBranch *br2 = tree->Branch("h2", "TH1F", &h2, 1280000, 0);
   TBranch *br3 = tree->Branch("h3", "TH1F", &h3, 1280000, 0);
 
-
   double ss1,ss2,ss3;
   int    ev, tr ;
   long int code;
@@ -289,9 +291,32 @@ void findTPCtracks(){
   double t_anod, tt, ll;
 
 
-  int start_bin, end_bin,  peak_bin;
-  double peak, start, end, energy, base;
-  UInt_t status;
+  int start_bin[3], end_bin[3], peak_bin[3];
+  double peak[3], start[3], end[3], energy[3], base[3], s_length[3];
+  double energy_tot;
+  UInt_t status[3];
+
+  reco = new TTree("reco", "reco_tree");
+  TBranch *bR_s1     = reco->Branch("status1", &status[0], "status1/I");
+  TBranch *bR_s2     = reco->Branch("status2", &status[1], "status2/I");
+  TBranch *bR_s3     = reco->Branch("status3", &status[2], "status3/I");
+  TBranch *bR_E1     = reco->Branch("E1", &energy[0], "E1/D");
+  TBranch *bR_E2     = reco->Branch("E2", &energy[1], "E2/D");
+  TBranch *bR_E3     = reco->Branch("E3", &energy[2], "E3/D");
+  TBranch *bR_ER     = reco->Branch("ER", &energy_tot, "ER/D");
+  TBranch *bR_start1 = reco->Branch("start1", &start[0], "start1/D");
+  TBranch *bR_start2 = reco->Branch("start2", &start[1], "start2/D");
+  TBranch *bR_start3 = reco->Branch("start3", &start[2], "start3/D");
+  TBranch *bR_stop1  = reco->Branch("stop1", &end[0],  "stop1/D");
+  TBranch *bR_stop2  = reco->Branch("stop2", &end[1],  "stop2/D");
+  TBranch *bR_stop3  = reco->Branch("stop3", &end[2],  "stop3/D");
+  TBranch *bR_peak1  = reco->Branch("peak1", &peak[0], "peak1/D");
+  TBranch *bR_peak2  = reco->Branch("peak2", &peak[1], "peak2/D");
+  TBranch *bR_peak3  = reco->Branch("peak3", &peak[2], "peak3/D");
+  TBranch *bR_base1  = reco->Branch("base1", &base[0], "base1/D");
+  TBranch *bR_base2  = reco->Branch("base2", &base[1], "base2/D");
+  TBranch *bR_base3  = reco->Branch("base3", &base[2], "base3/D");
+
 
 
 //==============================================================================
@@ -397,26 +422,17 @@ void findTPCtracks(){
 
       tree->Fill();
 
-      ss1 = get_sr(h1,start_bin, end_bin, peak_bin, peak, start, end, energy, status, base );
+      ss1 = get_sr(h1,start_bin[0], end_bin[0], peak_bin[0], peak[0], start[0], end[0], energy[0], status[0], base[0] );
+      ss2 = get_sr(h2,start_bin[1], end_bin[1], peak_bin[1], peak[1], start[1], end[1], energy[1], status[1], base[1] );
+      ss3 = get_sr(h3,start_bin[2], end_bin[2], peak_bin[2], peak[2], start[2], end[2], energy[2], status[2], base[2] );
 
-      std::cout << start_bin << "\t"  << end_bin << "\t" << peak_bin  << "\t" << start << "\t" << end << " "<< peak << "\t"<<energy << "\t"<< status << "\t"<< base << "\n";
-
-//      ss2 = get_sr(h2,start_bin, end_bin, peak_bin, peak, start, end, energy, status, base );
-//      ss3 = get_sr(h3,start_bin, end_bin, peak_bin, peak, start, end, energy, status, base );
-
-//      ss1 = check_fired(h1);
-//      ss2 = check_fired(h2);
-//      ss3 = check_fired(h3);
-
-//      ss1 = eval_base(h1);
-//      ss2 = eval_base(h2);
-//      ss3 = eval_base(h3);
-
-      //std::cout << h1->GetMaximumBin() << "\t" << h1->GetMaximum() << "\t" << h1->GetBinContent( h1->GetMaximumBin() ) << "\n";
-      //std::cout << h1->GetMean(1) << "\t" << h1->GetMeanError(1) << "\t" << h1->GetRMS(1) << " " << h1->GetRMSError(1) << "\n";
-      //std::cout << h1->GetMaximumBin() << "\t" << h1->GetMaximum() << "\t" << av(h1) << "\n";
-      //std::cout << avg(h1) << "\t"  << eval_base(h1) << "\t" << h1->GetMinimum()  << "\t" << h1->GetMaximum() <<  "\t" << check_fired( h1 ) << "\n";
-      //std::cout          << "\t\t"  << s1.base       << "\t" << s1.peak << "\t" << s1.start  << "\t" << s1.energy << "\n";
+      energy_tot = energy[0]+energy[1]+energy[2];
+      reco->Fill();
+      if(PRINT_RECO){
+        std::cout << start_bin[0] << "\t"  << end_bin[0] << "\t" << peak_bin[0]  << "\t" << start[0] << "\t" << end[0] << " "<< peak[0] << "\t"<<energy[0] << "\t"<< status[0] << "\t"<< base[0] << "\n";
+        std::cout << start_bin[1] << "\t"  << end_bin[1] << "\t" << peak_bin[1]  << "\t" << start[1] << "\t" << end[1] << " "<< peak[1] << "\t"<<energy[1] << "\t"<< status[1] << "\t"<< base[1] << "\n";
+        std::cout << start_bin[2] << "\t"  << end_bin[2] << "\t" << peak_bin[2]  << "\t" << start[2] << "\t" << end[2] << " "<< peak[2] << "\t"<<energy[2] << "\t"<< status[2] << "\t"<< base[2] << "\n\n";
+      }
 
       h1->Reset();
       h2->Reset();
@@ -461,6 +477,7 @@ void findTPCtracks(){
 
   file = new TFile(FILENAME,"RECREATE");
   tree->Write();
+  reco->Write();
   mc_true->Write();
   file->Write();
   file->Close();
